fa8a1ac18f0ecba503b498b10338667dd22ad2d8
xentixar <xentixar@gmail.com>
2024-02-24 09:53:08 +0545

    initialize project

---
+++ README.md
@@ -0,0 +1,1 @@
+## Tasks Tracker System
---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\ServiceProvider;
+
+class AppServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        //
+    }
+
+    public function boot(): void
+    {
+        Schema::defaultStringLength(191);
+        Paginator::useBootstrapFive();
+    }
+}
